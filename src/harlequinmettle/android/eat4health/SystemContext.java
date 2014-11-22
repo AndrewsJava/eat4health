937c2b74b21cc5af46d52cf4c9145725cbbbc721
andrew parelius <parelius@uw.edu>
2014-11-22 10:50:43 -0500

    started refactoring heathfoodconcepts

---
+++ src/harlequinmettle/android/eat4health/SystemContext.java
@@ -0,0 +1,20 @@
+package harlequinmettle.android.eat4health;
+
+import android.content.Context;
+
+public class SystemContext extends android.app.Application {
+	// require name in android manifest
+
+	// <application
+	// android:name="SystemContext"
+	private static SystemContext instance;
+
+	public SystemContext() {
+		instance = this;
+	}
+
+	public static Context getContext() {
+		return instance;
+	}
+
+}
